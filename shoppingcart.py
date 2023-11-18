f0ae64dcd8d2c8d587f90b1c609e985de2128b0e
HARSH ADHIKARI <harshadhikari023@gmail.com>
2023-11-18 12:59:05 +0530

    Add files via upload

---
+++ shoppingcart.py
@@ -0,0 +1,29 @@
+def gst(items,ui,gs,q):
+	max = 0
+	qwe = 0 
+	for i in range(len(ui)):
+		a = ui[i]*q[i]*gs[i]//100
+		if(a>max):
+			max = a
+			qwe = i
+	return items[qwe],max
+	
+
+def pay(ui,gs,q):
+	ans = 0
+	for i in range(len(ui)):
+		if(ui[i]>=500):
+			ans += ui[i] + ui[i]*q[i]*gs[i]//100-(5//100)
+		else:
+			ans += ui[i] + ui[i]*q[i]*gs[i]//100 
+	return ans
+
+items = ["Leather Wallet","Umbrella","Cigarette","Honey"]
+ui = [1100,900,200,100]
+gs = [18,12,28,0]
+q = [1,4,3,2]
+
+
+print("Max GST : " ,gst(items,ui,gs,q))
+
+print("Total amount to be paid : ",pay(ui,gs,q))
